513226bfea978915af27d5df7879e788e5f19c8b
M1xey <m.1.xey@ya.ru>
2015-05-03 21:33:12 +0300

    top_dobav.php пытаюсь подобрать нормальный запрос

--- top_dobav.php
+++ top_dobav.php
@@ -1,1 +1,23 @@
 Самые активные заполнители библиотеки
+<?php	
+	$query = "select count(id_reg) as count from resource order by count";
+	include_once("connect.php");
+	$q = mysql_query($query) or mysql_error();
+	try {
+		if ($q){
+			echo 'Результат запроса:<br>';
+			echo '<table border=1>';
+			while ($row = mysql_fetch_row($q)) {
+				echo '<tr>';
+				for ($i = 0; $i<count($row); $i++)
+					echo '<td>'.$row[$i].'</td>';
+				echo '</tr>';
+			}
+			echo '</table>';
+			mysql_free_result($q);
+		}
+	}
+	catch (Exception $e) {
+		echo "Exception";
+	}
+?>
